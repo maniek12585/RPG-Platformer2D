e382f17a5cf970d7c4d605b7a8ae626abcaf9298
Dylson <michal.dyla@o2.pl>
2019-12-12 21:35:12 +0100

    Zamiana postaci + design kota

---
+++ Game_project_combat_system_test/Assets/Scripts/CharacterSwitching.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSwitching : MonoBehaviour
+{
+    public GameObject _player1, _player2;
+    private short _charActive = 1;
+
+    void Start()
+    {
+        _player1.gameObject.SetActive(true);
+        _player2.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            _charActive++;
+
+            if (_charActive > 2)
+                _charActive = 1;
+        }
+
+        switch(_charActive)
+        {
+            case 1:
+                _player1.gameObject.SetActive(true);
+                _player2.gameObject.SetActive(false);
+                break;
+
+            case 2:
+                _player1.gameObject.SetActive(false);
+                _player2.gameObject.SetActive(true);
+                break;
+        }
+            
+    }
+}
---
+++ Game_project_combat_system_test/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    public float _speed;
+    public float _jumpForce;
+    private float _moveInput;
+
+    private bool _isGrounded;
+    public Transform _feetCollider;
+    public float _checkRadius;
+    public LayerMask _ground;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        _moveInput = Input.GetAxisRaw("Horizontal");
+        _rb.velocity = new Vector2(_moveInput * _speed, _rb.velocity.y);
+    }
+
+    void Update()
+    {
+        _isGrounded = Physics2D.OverlapCircle(_feetCollider.position, _checkRadius, _ground);
+
+        if (_isGrounded && Input.GetKeyDown(KeyCode.Space))
+            _rb.velocity = Vector2.up * _jumpForce;
+    }
+}
